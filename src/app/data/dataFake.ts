1dc2315bc0440046779f4f5bd6e5f5429b931a9f
Yuri <yuri.moreira803@gmail.com>
2023-09-22 01:02:47 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,50 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "O Poder dos Vilões da Marvel: De Loki a Thanos",
+        "description": "Explore o lado sombrio do universo Marvel enquanto mergulhamos nos perfis de alguns dos vilões mais icônicos, desde o astuto Loki até o temível Thanos. Descubra o que os torna tão fascinantes e ameaçadores para nossos heróis favoritos.",
+        "photoCover": "https://pm1.aminoapps.com/7649/0304f9a9628ad9ef040d89587a7abecdaff52c9er1-1000-500v2_uhq.jpg"
+    },
+    {
+        "id":"2",
+        "title": "A Evolução do Traje do Homem-Aranha: Dos Anos 60 aos Dias Atuais",
+        "description": "Acompanhe a jornada visual do icônico traje do Homem-Aranha desde sua criação nos quadrinhos nos anos 60 até suas adaptações mais recentes no cinema. Descubra como o design evoluiu ao longo do tempo.",
+        "photoCover": "https://images.virgula.me/2014/05/02/2503311174-infografico-homem-aranha-686396352.jpg"
+    },
+    {
+        "id":"3",
+        "title": "As Origens Místicas do Doutor Estranho: Da Cirurgia à Magia",
+        "description": "Descubra como o Doutor Estranho, originalmente um cirurgião arrogante, se transformou no Mago Supremo do Universo Marvel. Explore sua jornada de redenção e maestria das artes místicas.",
+        "photoCover": "https://i0.wp.com/geekpopnews.com.br/wp-content/uploads/2021/07/Dr-estranho.jpg?resize=1200%2C640&ssl=1"
+    },
+    {
+        "id":"4",
+        "title": "A Revolução da Diversidade na Marvel: Heróis que Refletem o Mundo Real",
+        "description": "Explore como a Marvel abraçou a diversidade e a representação em seus quadrinhos e filmes, apresentando heróis de origens diversas e lutas únicas. Descubra por que isso é importante para a indústria do entretenimento.",
+        "photoCover": "https://jamesons.com.br/wp-content/uploads/2020/05/roteirista-vingadores-fala-sobre-diversidade-capa.jpg"
+    },
+    {
+        "id":"5",
+        "title": "X-Men: A Saga da Fênix Negra - Poder, Tragédia e Redenção",
+        "description": "Explore a icônica história da Fênix Negra dos X-Men, que abrange quadrinhos, animações e filmes. Descubra como Jean Grey se tornou a Fênix Negra, desafiando seus próprios limites e ameaçando o mundo mutante.",
+        "photoCover": "https://www.einerd.com.br/wp-content/uploads/2017/04/xmenf%C3%AAnixnegraintro.jpg"
+    },
+    {
+        "id":"6",
+        "title": "Wolverine: O Carcaju e Sua Jornada Épica nos Quadrinhos e no Cinema",
+        "description": "Entre na mente do icônico Wolverine, também conhecido como Logan, enquanto exploramos sua trajetória nos quadrinhos e sua representação no cinema. Descubra por que esse anti-herói se tornou um favorito dos fãs.",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2021/08/wolverine-meme-capa-hq.jpg"
+    },
+    {
+        "id":"",
+        "title": "",
+        "description": "",
+        "photoCover": ""
+    },
+    {
+        "id":"",
+        "title": "",
+        "description": "",
+        "photoCover": ""
+    },
+]
